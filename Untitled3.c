8c7bcfbc9c2aea14c9da958aff0bb35607fff60b
LM9001 <95847352+LM9001@users.noreply.github.com>
2021-12-09 10:36:26 +0100

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(){
+    int broj;
+
+    printf("Unesi broj");
+    scanf("%d", &broj);
+
+   // expression?if true:if false;
+
+    printf(broj%2==0?"Broj je paran":"Broj je neparan");
+
+}
