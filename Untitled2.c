6dff501379435a5f477465d17e8d0890ad99cbc8
LM9001 <95847352+LM9001@users.noreply.github.com>
2021-12-09 09:37:24 +0100

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    int stranicaA=90;
+    int stranicaB=50;
+    int stranicaC=30;
+    int V=stranicaA*stranicaB*stranicaC;
+    printf("%d", V);
+    }
