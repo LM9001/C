d2180975f224353ee1e14a95860bdb34082e5712
LM9001 <95847352+LM9001@users.noreply.github.com>
2022-03-10 09:01:13 +0100

    Add files via upload

---
+++ if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main(){
+    int a;
+    printf("Unesi broj\n");
+    scanf("%d", &a);
+    if(a%2==0)
+        printf("broj je paran");
+    if(a%2==1)
+            printf("broj je neparan");
+
+}
